9b88641542eeacd1e3e2453b79e5c9d1d4a97875
ConstructBMS <constructbms@gmail.com>
2025-07-27 11:18:34 +0100

    fix: Add default exports to empty tab components to resolve import errors

---
+++ src/modules/ProgrammeManager/components/RibbonTabs/tabs/AllocationTab.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AllocationTab = () => {
+  return (
+    <div className="flex flex-row gap-3 p-2 bg-[#f9f9f9] border-t border-gray-200">
+      <div className="text-sm text-gray-500">Allocation Tab - Coming Soon</div>
+    </div>
+  );
+};
+
+export default AllocationTab;
---
+++ src/modules/ProgrammeManager/components/RibbonTabs/tabs/ProjectTab.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProjectTab = () => {
+  return (
+    <div className="flex flex-row gap-3 p-2 bg-[#f9f9f9] border-t border-gray-200">
+      <div className="text-sm text-gray-500">Project Tab - Coming Soon</div>
+    </div>
+  );
+};
+
+export default ProjectTab;
